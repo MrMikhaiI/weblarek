51ac432e2dc9ce2addd6ca4afe5b0d94fcf7c45e
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-19 18:00:56 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,43 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE'; 
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';  
 
-export type TPayment = 'card' | 'cash';  
+export type TPayment = 'online' | 'offline';   
 
-export interface IApi { 
-  get<T extends object>(uri: string): Promise<T>; 
-  post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>; 
+export interface IApi {  
+  get<T extends object>(uri: string): Promise<T>;  
+  post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;  
 }
 
-export interface IProduct { 
-  id: string;       // уникальный идентификатор товара 
-  title: string;    // название товара 
-  description: string; // описание товара 
-  image: string;    // ссылка на изображение товара 
-  category: string; // категория, к которой относится товар 
-  price: number;    // цена товара 
-} 
+export interface IProduct {  
+  id: string;       
+  title: string;    
+  description: string; 
+  image: string;    
+  category: string; 
+  price: number;    
+}  
 
-export interface IBuyer { 
-  payment: TPayment; // выбранный способ оплаты 
-  email: string;     // email покупателя 
-  phone: string;     // телефон покупателя 
-  address: string;   // адрес доставки 
-} 
+export interface IBuyer {  
+  payment: TPayment; 
+  email: string;     
+  phone: string;     
+  address: string;   
+}  
 
-export interface IOrder { 
-  items: IProduct[]; // массив товаров из корзины
-  buyer: IBuyer;     // данные покупателя
-} 
-
-export interface IApiProductsResponse { 
+export interface IApiProductsResponse {  
+  total: number;    
   items: IProduct[]; 
-} 
+}
 
-export interface IOrderResponse {}
+export interface IOrderRequest { 
+  payment: TPayment;       
+  email: string;
+  phone: string; 
+  address: string;
+  total: number;         
+  items: string[];         
+}
+ 
+export interface IOrderResponse { 
+  id: string;               
+  total: number;
+}
