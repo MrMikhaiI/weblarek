e1ad12b5edde8ca2d33afe09beda0e4c3fadfc21
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-19 18:00:26 +0300

    Update Communication.ts

--- src/components/models/Communication.ts
+++ src/components/models/Communication.ts
@@ -1,44 +1,20 @@
-import { Api } from '../base/Api'; 
-import { IApi, IProduct, IOrder, IBuyer } from '../../types';
-import { API_URL } from '../../utils/constants'; 
+import { Api } from '../base/Api';  
+import { IApi, IProduct, IApiProductsResponse, IOrderRequest, IOrderResponse } from '../../types';
+import { API_URL } from '../../utils/constants';  
 
-interface IApiProductsResponse { 
-  items: IProduct[]; 
-} 
+export class Communication {  
+  private api: IApi;   
 
-interface IOrderRequest {
-  items: IProduct[];
-  buyer: IBuyer;  
-}
-
-interface IOrderResponse {
-  success?: boolean;
-}
-
-export class Communication { 
-  private api: IApi;  
-
-  /** 
-   * Конструктор использует композицию: принимает IApi для запросов
-   * @param api 
-   */ 
-  constructor(api: IApi) { 
-    this.api = api; 
+  constructor(api: IApi) {  
+    this.api = api;  
   } 
 
-  /** Получение массива товаров с сервера 
-   * @returns Promise<IProduct[]> 
-   */ 
-  async getProductList(): Promise<IProduct[]> { 
-    const response = await this.api.get<IApiProductsResponse>(`${API_URL}/product/`); 
-    return response.items || []; 
+  async getProductList(): Promise<IProduct[]> {  
+    const response = await this.api.get<IApiProductsResponse>(`${API_URL}/product/`);  
+    return response.items;  
   } 
 
-  /** Отправка данных заказа на сервер 
-   * @param order - объект заказа {items: IProduct[], buyer: IBuyer}
-   * @returns Promise<IOrderResponse> 
-   */ 
-  async sendOrder(order: IOrder): Promise<IOrderResponse> {  
-    return await this.api.post(`${API_URL}/order/`, order); 
+  async sendOrder(orderRequest: IOrderRequest): Promise<IOrderResponse> {  
+    return await this.api.post<IOrderResponse>(`${API_URL}/order/`, orderRequest);  
   } 
 }
