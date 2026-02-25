660e0a653fd9660532c9d59fbd1ad26221edc5e2
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 19:51:21 +0300

    Update Basket.ts

--- src/components/views/Basket.ts
+++ src/components/views/Basket.ts
@@ -1,1 +1,39 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
+import { IEvents } from '../base/Events';
 
+interface IBasket {
+  items: HTMLElement[];
+  price: number;
+}
+
+export class Basket extends Component<IBasket> {
+  protected listElement!: HTMLElement;
+  protected orderButton!: HTMLButtonElement;
+  protected priceElement!: HTMLElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+    
+    this.listElement = ensureElement('.basket__list', container);
+    this.orderButton = ensureElement('.basket__button', container);
+    this.priceElement = ensureElement('.basket__price', container);
+    
+    this.orderButton.addEventListener('click', () => {
+      events.emit('order:open');
+    });
+  }
+
+  set items(items: HTMLElement[]) {
+    this.listElement.innerHTML = '';
+    if (items.length === 0) {
+      this.listElement.innerHTML = '<p class="basket__empty">Корзина пуста</p>';
+    } else {
+      this.listElement.append(...items);
+    }
+  }
+
+  set price(value: number) {
+    this.priceElement.textContent = `${value} синапсов`;
+  }
+}
