2d5113381c84f4396cbddcca18c259e12b2a8df7
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 19:55:31 +0300

    Update Success.ts

--- src/components/views/Success.ts
+++ src/components/views/Success.ts
@@ -1,1 +1,24 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
 
+interface ISuccess {
+  total: number;
+}
+
+export class Success extends Component<ISuccess> {
+  protected closeButton!: HTMLButtonElement;
+  protected totalElement!: HTMLElement;
+
+  constructor(container: HTMLElement, protected onClose: () => void) {
+    super(container);
+    
+    this.closeButton = ensureElement('.order-success__close', container);
+    this.totalElement = ensureElement('.order-success__description', container);
+    
+    this.closeButton.addEventListener('click', () => onClose());
+  }
+
+  set total(value: number) {
+    this.totalElement.textContent = `Списано ${value} синапсов`;
+  }
+}
