6f31667a4463ed9eb65cc7e5190ac585554d83e2
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 19:50:23 +0300

    Update Modal.ts

--- src/components/views/Modal.ts
+++ src/components/views/Modal.ts
@@ -1,1 +1,37 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
+import { IEvents } from '../base/Events';
 
+interface IModal {
+  content: HTMLElement;
+}
+
+export class Modal extends Component<IModal> {
+  protected closeButton!: HTMLButtonElement;
+  protected contentElement!: HTMLElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+    
+    this.closeButton = ensureElement('.modal__close', container);
+    this.contentElement = ensureElement('.modal__content', container);
+    
+    this.closeButton.addEventListener('click', () => this.close());
+    container.addEventListener('click', (e) => {
+      if (e.target === container) this.close();
+    });
+  }
+
+  set content(element: HTMLElement) {
+    this.contentElement.replaceChildren(element);
+  }
+
+  open() {
+    this.container.classList.add('modal_active');
+  }
+
+  close() {
+    this.container.classList.remove('modal_active');
+    this.contentElement.innerHTML = '';
+  }
+}
