5e8e1f6071435b6692393d1815ce19490ab151f5
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 21:58:11 +0300

    Update CardCatalog.ts

--- src/components/views/Card/CardCatalog.ts
+++ src/components/views/Card/CardCatalog.ts
@@ -1,11 +1,10 @@
-import { Card, TCardCatalog } from './Card';
+import { Card } from '../Card';
 import { categoryMap, CDN_URL } from '../../../utils/constants';
 import { IEvents } from '../../base/Events';
+import { IProduct } from '../../../types';
 import { ensureElement } from '../../../utils/utils';
 
-export type TCardCatalog = Pick<IProduct, 'image' | 'category'>;
-
-export class CardCatalog extends Card<TCardCatalog> {
+export class CardCatalog extends Card {
   protected imageElement!: HTMLImageElement;
   protected categoryElement!: HTMLElement;
 
@@ -13,9 +12,10 @@ export class CardCatalog extends Card<TCardCatalog> {
     super(container);
     this.imageElement = ensureElement('.card__image', container);
     this.categoryElement = ensureElement('.card__category', container);
-    
+
     container.addEventListener('click', () => {
-      events.emit('productCard:click', {}); // id через dataset
+      const productId = (container as HTMLElement).dataset.productId;
+      this.events.emit('products:select', productId);
     });
   }
 
