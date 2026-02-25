1b08d0cf7addd326d1de0e12db130ccc007504aa
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 21:59:05 +0300

    Update CardBasket.ts

--- src/components/views/Card/CardBasket.ts
+++ src/components/views/Card/CardBasket.ts
@@ -1,1 +1,21 @@
+import { Card } from '../Card';
+import { ensureElement } from '../../../utils/utils';
+import { IProduct } from '../../../types';
 
+interface ICardBasketCallbacks {
+  onDelete: (id: string) => void;
+}
+
+export class CardBasket extends Card {
+  protected deleteButton!: HTMLButtonElement;
+
+  constructor(container: HTMLElement, protected callbacks: ICardBasketCallbacks) {
+    super(container);
+    
+    this.deleteButton = ensureElement('.cart-item__delete', container);
+    this.deleteButton.addEventListener('click', () => {
+      const productId = (container as HTMLElement).dataset.productId;
+      this.callbacks.onDelete(productId!);
+    });
+  }
+}
