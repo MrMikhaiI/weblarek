9441efd2e8d0e7849a20268e7d9e2a4ad5fafd08
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 19:54:03 +0300

    Update Form.ts

--- src/components/views/Form/Form.ts
+++ src/components/views/Form/Form.ts
@@ -1,1 +1,41 @@
+import { Component } from '../../base/Component';
+import { ensureElement } from '../../../utils/utils';
+import { IEvents } from '../../base/Events';
 
+export interface IForm {
+  valid: boolean;
+  errors: string[];
+}
+
+export class Form extends Component<IForm> {
+  protected formElement!: HTMLFormElement;
+  protected submitButton!: HTMLButtonElement;
+  protected errorsElement!: HTMLElement;
+
+  constructor(container: HTMLFormElement, protected events: IEvents) {
+    super(container);
+    
+    this.formElement = container;
+    this.submitButton = ensureElement('button[type="submit"]', container);
+    this.errorsElement = ensureElement('.form__errors', container);
+    
+    this.submitButton.disabled = true;
+    this.submitButton.classList.add('button_disabled');
+    
+    this.formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (!this.submitButton.disabled) {
+        this.events.emit(`${this.formElement.name}:submit`);
+      }
+    });
+  }
+
+  set valid(value: boolean) {
+    this.submitButton.disabled = !value;
+    this.submitButton.classList.toggle('button_disabled', !value);
+  }
+
+  set errors(errors: string[]) {
+    this.errorsElement.textContent = errors.join(', ');
+  }
+}
