55af297bb1731f94274157609dc751c7d5a275fd
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 19:44:23 +0300

    Update Header.ts

--- src/components/views/Header.ts
+++ src/components/views/Header.ts
@@ -1,1 +1,22 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
+import { IEvents } from '../base/Events';
 
+export class Header extends Component<{ counter: number }> {
+  protected basketButton!: HTMLButtonElement;
+  protected counterElement!: HTMLElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+    this.basketButton = ensureElement('.header__basket', container);
+    this.counterElement = ensureElement('.header__basket-counter', container);
+    
+    this.basketButton.addEventListener('click', () => {
+      events.emit('cart:open');
+    });
+  }
+
+  set counter(value: number) {
+    this.counterElement.textContent = String(value);
+  }
+}
