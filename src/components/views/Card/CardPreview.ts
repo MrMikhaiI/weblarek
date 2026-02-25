2452158d4395fb6305245006ce84235a311b5c7b
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-25 21:58:41 +0300

    Update CardPreview.ts

--- src/components/views/Card/CardPreview.ts
+++ src/components/views/Card/CardPreview.ts
@@ -1,1 +1,34 @@
+import { Card } from '../Card';
+import { ensureElement } from '../../../utils/utils';
+import { IProduct } from '../../../types';
 
+interface ICardPreviewCallbacks {
+  onClick: () => void;
+}
+
+export class CardPreview extends Card {
+  protected descriptionElement!: HTMLElement;
+  protected button!: HTMLButtonElement;
+
+  constructor(container: HTMLElement, protected callbacks: ICardPreviewCallbacks) {
+    super(container);
+    
+    this.descriptionElement = ensureElement('.card__text', container);
+    this.button = ensureElement('.card__button', container);
+    
+    this.button.addEventListener('click', () => callbacks.onClick());
+  }
+
+  render(product: IProduct) {
+    super.render(product);
+    this.descriptionElement.textContent = product.description || '';
+  }
+
+  setButtonText(text: string) {
+    this.button.textContent = text;
+    this.button.disabled = !product.price; 
+    if (!product.price) {
+      this.button.textContent = 'Недоступно';
+    }
+  }
+}
