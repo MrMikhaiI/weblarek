c7c93f7473379542028cfb60a676cc49f9c76dfb
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-26 15:20:51 +0300

    Update types.ts

--- src/components/views/Card/types.ts
+++ src/components/views/Card/types.ts
@@ -1,1 +1,12 @@
+export interface ICardCatalogCallbacks {
+  onSelect: (id: string) => void;
+}
+
+export interface ICardBasketCallbacks {
+  onDelete: (id: string) => void;  // ✅ onDelete!
+}
+
+export interface ICardPreviewCallbacks {
+  onClick: () => void;
+}
 
