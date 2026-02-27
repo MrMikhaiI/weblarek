68acab9bf38eb743106b100842aaa83337e7f94e
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-27 10:30:37 +0300

    Update Communication.ts

--- src/components/models/Communication.ts
+++ src/components/models/Communication.ts
@@ -1,19 +1,19 @@
-import { IApi, IProduct, IOrderRequest, IOrderResponse } from '../../types'; 
+import { IApi, IProduct, IOrderRequest, IOrderResponse } from '../../types';  
 
-export class Communication {  
-  private api: IApi;   
+export class Communication {   
+  private api: IApi;    
 
-  constructor(api: IApi) {  
-    this.api = api;  
-  } 
+  constructor(api: IApi) {   
+    this.api = api;   
+  }  
 
-  async getProductList(): Promise<IProduct[]> {  
-    const response = await this.api.get(`${API_URL}/product/`);  
-    return (response as any).items as IProduct[];  
-  } 
+  async getProductList(): Promise<IProduct[]> {   
+    const response = await this.api.get('product/');   
+    return (response as any).items as IProduct[];   
+  }  
 
-  async sendOrder(orderRequest: IOrderRequest): Promise<IOrderResponse> {  
-    const response = await this.api.post(`${API_URL}/order/`, orderRequest);  
-    return response as IOrderResponse;  
-  } 
+  async sendOrder(orderRequest: IOrderRequest): Promise<IOrderResponse> {   
+    const response = await this.api.post('order/', orderRequest);   
+    return response as IOrderResponse;   
+  }  
 }
