3de0d0854c54e5bad6c12a5495fe358e261f1e87
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-27 10:32:24 +0300

    Update CardBasket.ts

--- src/components/views/Card/CardBasket.ts
+++ src/components/views/Card/CardBasket.ts
@@ -1,18 +1,17 @@
-import { Card } from './index';
-import { ensureElement } from '../../../utils/utils';
-import { ICardBasketCallbacks } from './types';
-import { IProduct } from '../../../types';
+import { Card } from './index'; 
+import { ensureElement } from '../../../utils/utils'; 
+import { ICardBasketCallbacks } from './types'; 
+import { IProduct } from '../../../types'; 
 
-export class CardBasket extends Card<IProduct> {  
-  protected deleteButton!: HTMLButtonElement;
+export class CardBasket extends Card<IProduct> {   
+  protected deleteButton!: HTMLButtonElement; 
 
-  constructor(container: HTMLElement, protected callbacks: ICardBasketCallbacks) {
-    super(container);
-    
-    this.deleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', container);
-    this.deleteButton.addEventListener('click', () => {
-      const productId = container.dataset.productId;
-      this.callbacks.onDelete(productId!);
-    });
-  }
+  constructor(container: HTMLElement, protected callbacks: ICardBasketCallbacks) { 
+    super(container); 
+     
+    this.deleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', container); 
+    this.deleteButton.addEventListener('click', () => { 
+      this.callbacks.onDelete(container.dataset.productId!); 
+    }); 
+  } 
 }
