a6caa79b7f1310b3fbd65f01eb9038b1b7f49277
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-27 10:34:02 +0300

    Update OrderForm.ts

--- src/components/views/Form/OrderForm.ts
+++ src/components/views/Form/OrderForm.ts
@@ -1,49 +1,47 @@
-import { Form } from './Form';
-import { ensureElement } from '../../../utils/utils';
-import { IEvents } from '../../base/Events';
+import { ensureElement } from '../../../utils/utils'; 
+import { IEvents } from '../../base/Events'; 
 
-export class OrderForm extends Form {
-  protected paymentButtons!: NodeListOf<HTMLButtonElement>;
-  protected addressInput!: HTMLInputElement;
+export class OrderForm extends Form { 
+  protected paymentButtons!: NodeListOf<HTMLButtonElement>; 
+  protected addressInput!: HTMLInputElement; 
 
-  constructor(container: HTMLFormElement, events: IEvents) {
-    super(container, events);
-    
-    this.paymentButtons = container.querySelectorAll('button[name="payment"]');
-    this.addressInput = ensureElement<HTMLInputElement>('input[name="address"]', container);
+  constructor(container: HTMLFormElement, events: IEvents) { 
+    super(container, events); 
+     
+    this.paymentButtons = container.querySelectorAll('.button.button_alt'); 
+    this.addressInput = ensureElement<HTMLInputElement>('input[name="address"]', container); 
+     
+    this.paymentButtons.forEach(btn => { 
+      btn.addEventListener('click', () => { 
+        events.emit('order:changed', {  
+          field: 'payment',  
+          value: btn.dataset.payment!  
+        }); 
+      }); 
+    }); 
+     
+    this.addressInput.addEventListener('input', () => { 
+      events.emit('order:changed', {  
+        field: 'address',  
+        value: this.addressInput.value  
+      }); 
+    }); 
 
-    
-    this.paymentButtons.forEach(btn => {
-      btn.addEventListener('click', () => {
-        events.emit('order:changed', { 
-          field: 'payment', 
-          value: btn.dataset.payment! 
-        });
-      });
-    });
-    
-    this.addressInput.addEventListener('input', () => {
-      events.emit('order:changed', { 
-        field: 'address', 
-        value: this.addressInput.value 
-      });
-    });
+    this.formElement.addEventListener('submit', (e) => { 
+      e.preventDefault(); 
+      if (!this.submitButton.disabled) { 
+        events.emit('order:next');   
+      } 
+    }); 
+  } 
 
-    this.formElement.addEventListener('submit', (e) => {
-      e.preventDefault();
-      if (!this.submitButton.disabled) {
-        events.emit('order:next');  
-      }
-    });
-  }
+  set payment(value: string) { 
+    this.paymentButtons.forEach(btn => { 
+      btn.classList.toggle('button_alt-active', btn.dataset.payment === value); 
+    }); 
+  } 
 
-  set payment(value: string) {
-    this.paymentButtons.forEach(btn => {
-      btn.classList.toggle('button_alt-active', btn.dataset.payment === value);
-    });
-  }
-
-  set address(value: string) {
-    this.addressInput.value = value;
-  }
+  set address(value: string) { 
+    this.addressInput.value = value; 
+  } 
 }
