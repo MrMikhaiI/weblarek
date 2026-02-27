40b342edc5afaa27ec7e0a91ae745dde7df4a0be
Mikhail Scherbakov <mikhail.sc@yandex.ru>
2026-02-27 10:04:04 +0300

    Update CardBasket.ts

--- src/components/views/Card/CardBasket.ts
+++ src/components/views/Card/CardBasket.ts
@@ -9,7 +9,7 @@ export class CardBasket extends Card<IProduct> {
   constructor(container: HTMLElement, protected callbacks: ICardBasketCallbacks) {
     super(container);
     
-    this.deleteButton = ensureElement<HTMLButtonElement>('.cart-item__delete', container);
+    this.deleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', container);
     this.deleteButton.addEventListener('click', () => {
       const productId = container.dataset.productId;
       this.callbacks.onDelete(productId!);
